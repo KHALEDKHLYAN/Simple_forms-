65ff6689afee6e2efa1b0e04d78b588b4993dbd8
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2024-09-14 09:40:22 -0700

    ecomerce form

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:ecommerce_app/res/styles/appstyles.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppStyles.bgColor,
+    );
+  }
+}
